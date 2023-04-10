f97c786a14836512eaa9d5ec76e60f3eaf514c7f
galkinnikolay <galkin_nikolay@icloud.com>
2023-04-10 14:05:54 +0300

    Задача2 решение1.2

--- Zadacha2/Program.cs
+++ Zadacha2/Program.cs
@@ -4,6 +4,8 @@
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
 
+int max = 0;
+int min = 0;
 Console.Write("Введите a: ");
 string text1 = Console.ReadLine();
 int number1 = Convert.ToInt32(text1);
@@ -13,11 +15,12 @@ int number2 = Convert.ToInt32(text2);
 
 if (number1 < number2)
 {
- Console.Write("Max число: ");
- Console.Write(number2);
+ max = number2;
+ min = number1;
 }
 else
 {
- Console.Write("Max число: ");
- Console.Write(number1);
+ max = number1;
+ min = number2;
 }
+Console.WriteLine("max = " + max + " и " + "min = " + min);
