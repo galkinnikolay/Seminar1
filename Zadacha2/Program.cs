54daf200e03774108997b21bf5922d0b16464661
galkinnikolay <galkin_nikolay@icloud.com>
2023-04-10 13:37:23 +0300

    Задача2 решение

--- Zadacha2/Program.cs
+++ Zadacha2/Program.cs
@@ -4,3 +4,20 @@
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
 
+Console.Write("Введите a: ");
+string text1 = Console.ReadLine();
+int number1 = Convert.ToInt32(text1);
+Console.Write("Введите b: ");
+string text2 = Console.ReadLine();
+int number2 = Convert.ToInt32(text2);
+
+if (number1 < number2)
+{
+ Console.WriteLine("Max число: ");
+ Сonsole.WriteLine(number2);
+}
+else
+{
+ Console.WriteLine("Max число: ");
+ Console.WriteLine(number1); 
+}
